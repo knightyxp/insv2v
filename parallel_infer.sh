730847d4028b7ffa6408e4270b155b41351b4d3a
Xiangpeng Yang <knightyxp@gmail.com>
2025-11-09 04:58:03 +1100

    insv2v

--- parallel_infer.sh
+++ parallel_infer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-torchrun --nproc_per_node=4 /home/xianyang/Data/code/instruct-video-to-video/insv2v_parallel_json.py \
+torchrun --nproc_per_node=4 insv2v_parallel_json.py \
   --test_json /scratch3/yan204/yxp/VideoX_Fun/data/test_json/4tasks_rem_add_swap_local-style_test.json \
   --output_dir ./results/insv2v_on_our_bench_test \
-  --ckpt_path /instruct-v2v-5b.pth \
+  --ckpt_path ./insv2v.pth \
   --config_path /home/xianyang/Data/code/instruct-video-to-video/configs/instruct_v2v.yaml \
   --seed 0 \
   --num_frames 33 \
